d36ed691b80aa2859a58f5e5145bc0d0b9ac005a
marcjpm <marcjpm@gmail.com>
2022-03-24 11:41:51 +0100

    added first map

---
+++ maps/test/data/waypoints_1.js
@@ -0,0 +1,18 @@
+var json_waypoints_1 = {
+"type": "FeatureCollection",
+"name": "waypoints_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "ele": 84.00274658203125, "time": "2021-04-27T13:16:30.000", "cmt": "compass heading: 70.25527954101562deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-16-30.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14789953, 41.10238893 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "ele": 84.279296875, "time": "2021-04-27T13:16:53.000", "cmt": "compass heading: 27.898727416992188deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-16-53.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14820901, 41.1024105 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "ele": 81.81903076171875, "time": "2021-04-27T13:17:26.000", "cmt": "compass heading: 312.6985778808594deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-17-26.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14836998, 41.10266959 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "ele": 84.2432861328125, "time": "2021-04-27T13:17:47.000", "cmt": "compass heading: 339.50726318359375deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-17-47.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14836538, 41.10286004 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "ele": 83.84814453125, "time": "2021-04-27T13:18:02.000", "cmt": "compass heading: 241.88369750976562deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-18-02.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14833016, 41.10301004 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "ele": 77.789306640625, "time": "2021-04-27T13:18:45.000", "cmt": "compass heading: 248.31222534179688deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-18-45.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.147747, 41.10301357 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "ele": 77.9471435546875, "time": "2021-04-27T13:19:05.000", "cmt": "compass heading: 153.83030700683594deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-19-05.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14748927, 41.10303951 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "ele": 80.58251953125, "time": "2021-04-27T13:19:16.000", "cmt": "compass heading: 155.4620819091797deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-19-160.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14749561, 41.10299947 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "ele": 85.23052978515625, "time": "2021-04-27T13:19:45.000", "cmt": "compass heading: 176.1588134765625deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-19-45.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14745294, 41.10263596 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "ele": 94.06219482421875, "time": "2021-04-27T13:24:12.000", "cmt": "compass heading: 65.87358856201172deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-24-12.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14727561, 41.10249246 ] } },
+{ "type": "Feature", "properties": { "fid": "11", "ele": 86.10345458984375, "time": "2021-04-27T13:24:40.000", "cmt": "compass heading: 50.84807205200195deg\n\t\t\tcompass accuracy: 0", "link1_text": "2021-04-27_15-24-40.jpg" }, "geometry": { "type": "Point", "coordinates": [ 1.14775977, 41.10242821 ] } }
+]
+}
